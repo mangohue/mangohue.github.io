155a8674875f39e31702d67752a74831816cce70
smartmango <smartmango@qq.com>
2018-03-23 15:02:44 +0800

    sdsa

--- js/test.js
+++ js/test.js
@@ -1,23 +1,167 @@
-
-function tab(e){
-	console.log(e);
-	setTab(e.dataset.tab);
-		
-}
-
-function setTab(tabName){
-	var tab = document.getElementsByClassName("f-tab");
-	var body_tab = document.getElementsByClassName("body-tab");
 	
-	console.log(body_tab);
-	for (var i= 0;i<tab.length;i++) {
-		if(tab[i].dataset.tab == tabName){
-			tab[i].lastElementChild.className="tab-txt-true";
-			body_tab[i].hidden="";
-		}else{
-			tab[i].lastElementChild.className="tab-txt-false";
-			body_tab[i].hidden="hidden";
-		}
-		
+function setNormalHeight(){
+	var normal = document.getElementsByClassName("normal");
+	var height = window.getComputedStyle(normal[0]).width;
+	for(var i = 0; i<normal.length; i++){
+		normal[i].style.height = height;
+		normal[i].style.width = height;
 	}
 }
+
+setNormalHeight();
+
+new Vue({
+ 	el: '#page',
+  data:{
+		isPlay:true,
+		btn_switch:true,
+		rangeBright:100,
+		rangeSpeed:50,
+		currentColor:"#FFFFFF",
+		normal:[ 
+			{color:"#FF0000"},
+			{color:"#00FF00"},
+			{color:"#0000FF"},
+			{color:"#FFFF00"},
+			{color:"#FF00FF"},
+			{color:"#00FFFF"},
+			{color:"#FFFFFF"}
+		],
+		color_picker: [
+      {
+        	flex: 1,
+        	values: [
+        		"Dinner Mode","Holiday Mode","Meeting Mode","Party Mode",
+        		"Three color jumping change","Seven color jumping change","Three color breathing lamp",
+        		"Seven color breathing lamp","Red breathing lamp","Green breathing lamp",
+        		"Blue breathing lamp","Yellow breathing lamp","Cyan breathing lamp",
+        		"Purple breathing lamp", "White breathing lamp","Red and green breathing lamp",
+	      		"Red and blue breathing lamp","Blue and green breathing lamp","Seven color flashing",
+	      		"Red flashing","Blue flashing", " Green flashing",
+        		"Yellow flashing", "Cyan flashing","purple flashing","White flashing"
+        	],
+        	className: 'slot',
+        	textAlign: 'center',
+        	textSize: '10px',
+      }, 
+	 	],
+  	changeMode:function(e){
+			var changeMode = document.getElementsByClassName("change-mode");
+			changeMode[0].className="change-mode change-mode-left";
+			changeMode[1].className="change-mode"
+			changeMode[2].className="change-mode change-mode-right"
+			if(e == "jump"){
+				changeMode[0].className="change-mode change-mode-left change-mode-true";
+			}else if(e == "breath"){
+				changeMode[1].className="change-mode change-mode-true";
+			}else if(e == "falsh"){
+				changeMode[2].className="change-mode change-mode-right change-mode-true";
+			}
+		},
+		music_picker: [
+    	{
+     		flex: 1,
+     		values: [
+     			"Jump Blues","Fplk-Rock","Free Jazz","Trance","Pop-Rap","File Music"
+     		],
+	    	className: 'slot',
+      	textAlign: 'center',
+      	textSize: '10px',
+    	}, 
+		]
+	},
+	methods:{
+		tabClick:function(tabName){
+			var tab = document.getElementsByClassName("f-tab");
+			var tabImg = document.getElementsByClassName("f-img");
+			var body_tab = document.getElementsByClassName("body-tab");
+					
+			tabImg[0].src="images/try/tab_color_false.png";
+			tabImg[1].src="images/try/tab_mode_false.png";
+			tabImg[2].src="images/try/tab_music_false.png";
+			tabImg[3].src="images/try/tab_timer_false.png";
+					
+			for (var i= 0;i<tab.length;i++) { 
+				if(tab[i].dataset.tab == tabName){ 
+					tab[i].lastElementChild.className = "tab-txt-true"
+					body_tab[i].hidden="";
+				}else {
+					tab[i].lastElementChild.className = "tab-txt-false"
+					body_tab[i].hidden="hidden";
+				}
+			}
+			var btn = document.getElementById("btn-switch");
+			if(tabName == "timer"){
+				btn.hidden="hidden";
+			}else{
+				btn.hidden="";
+			}
+					
+			if(tabName == "color"){
+				tabImg[0].src="images/try/tab_color_true.png";
+			}else if(tabName == "mode"){
+				tabImg[1].src="images/try/tab_mode_true.png";
+			}else if(tabName == "music"){
+				tabImg[2].src="images/try/tab_music_true.png";
+			}else if(tabName == "timer"){
+				tabImg[3].src="images/try/tab_timer_true.png";
+			}
+		},
+		tabTitle:function(e){
+			var classic = document.getElementById("classic");
+			var custom = document.getElementById("custom");
+			var tabTitleClassic = document.getElementById("tab-title-classic");
+			var tabTitleCustom = document.getElementById("tab-title-custom");
+			var modeCurrent = document.getElementById("custom-current-color");
+			if(e == "classic"){
+				classic.hidden="";
+			custom.hidden="hidden";
+			tabTitleClassic.className="tab-title-left-true";
+				tabTitleCustom.className="tab-title-right-flase";
+				modeCurrent.hidden="hidden";
+			}else if(e == "custom"){
+				classic.hidden="hidden";
+				custom.hidden="";
+				tabTitleClassic.className="tab-title-left-flase";
+				tabTitleCustom.className="tab-title-right-true";
+				modeCurrent.hidden="";
+			}
+			var tabtitleMusic = document.getElementById("tab-title-music");
+			var tabtitleMic = document.getElementById("tab-title-mic");
+			var musicMain = document.getElementById("music-main");
+			var micMain = document.getElementById("mic-main");
+			if(e == "music"){
+				musicMain.hidden="";
+				micMain.hidden="hidden";
+				tabtitleMusic.className="tab-title-left-true";
+				tabtitleMic.className="tab-title-right-flase";
+			}else if(e == "mic"){
+				musicMain.hidden="hidden";
+				micMain.hidden="";
+				tabtitleMusic.className="tab-title-left-flase";
+				tabtitleMic.className="tab-title-right-true";
+			}
+		},
+		//音乐暂停开始
+		musicChange:function(e){
+			this._data.isPlay = !this._data.isPlay;
+		},
+	      		
+		//灯带开关切换
+		btnSwitch:function(e){
+			this._data.btn_switch = !this._data.btn_switch;
+		},
+		
+		//点击color页面normal颜色
+		normalColor: function (e){
+			this._data.currentColor = e;
+		},
+		
+		//mode页面picker change
+		modeChange:function(picker, values){
+			console.log(picker);
+			console.log(values);
+		}
+	}
+})
+
--- js/try.js
+++ js/try.js
@@ -1,26 +1,214 @@
-import Vue from 'vue'
-import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css'
-import App from './App.vue'
+	
+function setNormalHeight(){
+	var normal = document.getElementsByClassName("normal");
+	var height = window.getComputedStyle(normal[0]).width;
+	for(var i = 0; i<normal.length; i++){
+		normal[i].style.height = height;
+		normal[i].style.width = height;
+	}
+}
 
-Vue.use(MintUI)
+setNormalHeight();
 
 new Vue({
-      el: '#app',
-      methods: {
-        test: function() {
-          this.$toast('Hello world!')
-        },
-
-      }
-    })
-    new Vue({
-      el: '#test',
-      methods: {
-        test: function() {
-          this.$toast('test')
-        },
-
-      }
-    })
+ 	el: '#page',
+  data:{
+  		isSetTimer:true,
+		musicIsPlay:false,
+		micIsPlay:false,
+		btn_switch:true,
+		rangeBright:100,
+		rangeSpeed:50,
+		currentColor:"#FFFFFF",
+		normal:[ 
+			{color:"#FF0000"},
+			{color:"#00FF00"},
+			{color:"#0000FF"},
+			{color:"#FFFF00"},
+			{color:"#FF00FF"},
+			{color:"#00FFFF"},
+			{color:"#FFFFFF"}
+		],
+		color_picker: [
+      		{
+        		flex: 1,
+        		values: [
+        			"Dinner Mode","Holiday Mode","Meeting Mode","Party Mode",
+        			"Three color jumping change","Seven color jumping change","Three color breathing lamp",
+        			"Seven color breathing lamp","Red breathing lamp","Green breathing lamp",
+        			"Blue breathing lamp","Yellow breathing lamp","Cyan breathing lamp",
+        			"Purple breathing lamp", "White breathing lamp","Red and green breathing lamp",
+	      			"Red and blue breathing lamp","Blue and green breathing lamp","Seven color flashing",
+	      			"Red flashing","Blue flashing", " Green flashing",
+        			"Yellow flashing", "Cyan flashing","purple flashing","White flashing"
+        		],
+        		className: 'slot',
+        		textAlign: 'center',
+        		textSize: '10px',
+      		}, 
+	 	],
+  		changeMode:function(e){
+			var changeMode = document.getElementsByClassName("change-mode");
+			changeMode[0].className="change-mode change-mode-left";
+			changeMode[1].className="change-mode"
+			changeMode[2].className="change-mode change-mode-right"
+			if(e == "jump"){
+				changeMode[0].className="change-mode change-mode-left change-mode-true";
+				console.log("change to jumping");
+			}else if(e == "breath"){
+				changeMode[1].className="change-mode change-mode-true";
+				console.log("change to breathing");
+			}else if(e == "falsh"){
+				changeMode[2].className="change-mode change-mode-right change-mode-true";
+				console.log("change to falshing");
+			}
+		},
+		music_picker: [
+    	{
+     		flex: 1,
+     		values: [
+     			"Jump Blues","Fplk-Rock","Free Jazz","Trance","Pop-Rap","File Music"
+     		],
+	    	className: 'slot',
+      	textAlign: 'center',
+      	textSize: '10px',
+    	}, 
+		]
+	},
+	methods:{
+		tabClick:function(tabName){
+			var tab = document.getElementsByClassName("f-tab");
+			var tabImg = document.getElementsByClassName("f-img");
+			var body_tab = document.getElementsByClassName("body-tab");
+					
+			tabImg[0].src="images/try/tab_color_false.png";
+			tabImg[1].src="images/try/tab_mode_false.png";
+			tabImg[2].src="images/try/tab_music_false.png";
+			tabImg[3].src="images/try/tab_timer_false.png";
+					
+			for (var i= 0;i<tab.length;i++) { 
+				if(tab[i].dataset.tab == tabName){ 
+					tab[i].lastElementChild.className = "tab-txt-true"
+					body_tab[i].hidden="";
+				}else {
+					tab[i].lastElementChild.className = "tab-txt-false"
+					body_tab[i].hidden="hidden";
+				}
+			}
+			var btn = document.getElementById("btn-switch");
+			if(tabName == "timer"){
+				btn.hidden="hidden";
+			}else{
+				btn.hidden="";
+			}
+					
+			if(tabName == "color"){
+				tabImg[0].src="images/try/tab_color_true.png";
+			}else if(tabName == "mode"){
+				tabImg[1].src="images/try/tab_mode_true.png";
+			}else if(tabName == "music"){
+				tabImg[2].src="images/try/tab_music_true.png";
+			}else if(tabName == "timer"){
+				tabImg[3].src="images/try/tab_timer_true.png";
+			}
+		},
+		tabTitle:function(e){
+			var classic = document.getElementById("classic");
+			var custom = document.getElementById("custom");
+			var tabTitleClassic = document.getElementById("tab-title-classic");
+			var tabTitleCustom = document.getElementById("tab-title-custom");
+			var modeCurrent = document.getElementById("custom-current-color");
+			if(e == "classic"){
+				classic.hidden="";
+			custom.hidden="hidden";
+			tabTitleClassic.className="tab-title-left-true";
+				tabTitleCustom.className="tab-title-right-flase";
+				modeCurrent.hidden="hidden";
+			}else if(e == "custom"){
+				classic.hidden="hidden";
+				custom.hidden="";
+				tabTitleClassic.className="tab-title-left-flase";
+				tabTitleCustom.className="tab-title-right-true";
+				modeCurrent.hidden="";
+			}
+			var tabtitleMusic = document.getElementById("tab-title-music");
+			var tabtitleMic = document.getElementById("tab-title-mic");
+			var musicMain = document.getElementById("music-main");
+			var micMain = document.getElementById("mic-main");
+			if(e == "music"){
+				musicMain.hidden="";
+				micMain.hidden="hidden";
+				tabtitleMusic.className="tab-title-left-true";
+				tabtitleMic.className="tab-title-right-flase";
+			}else if(e == "mic"){
+				musicMain.hidden="hidden";
+				micMain.hidden="";
+				tabtitleMusic.className="tab-title-left-flase";
+				tabtitleMic.className="tab-title-right-true";
+			}
+		},
+		//音乐暂停开始
+		musicChange:function(e){
+			if(e == "pre"){
+				console.log("music pre");
+				this._data.musicIsPlay = true;
+			}else if(e == "start"){
+				this._data.musicIsPlay = !this._data.musicIsPlay;
+				console.log("music start");
+			}else if(e == "stop"){
+				this._data.musicIsPlay = !this._data.musicIsPlay;
+				console.log("music stop");
+			}else if(e == "next"){
+				console.log("music next");
+				this._data.musicIsPlay = true;
+			}
+		},
+		
+		//mic暂停开始
+		micChange:function(e){
+			 this._data.micIsPlay = !this._data.micIsPlay;
+			 if(e == true){
+			 	console.log("mic stop");
+			 }else{
+			 	console.log("mic start");
+			 }
+		},
+		
+		//灯带开关切换
+		btnSwitch:function(e){
+			this._data.btn_switch = !this._data.btn_switch;
+			if(e == false){
+				console.log("open LED");
+			}else{
+				console.log("close LED");
+			}
+		},
+		
+		//点击color页面normal颜色
+		normalColor: function (e){
+			this._data.currentColor = e;
+			console.log("set color to "+ e);
+		},
+		
+		//mode页面picker change
+		pickerModeChange(picker, values){
+			console.log("click "+values[0]);
+		},
+		
+		//music页面picker change
+		musicModeChange(picker, values){
+			console.log("click "+values[0]);
+		},
+		
+		//设置亮度
+		brightChange:function(value){
+			console.log("bright set to " + value);
+		},
+		
+		//设置速度
+		speedChange:function(value){
+			console.log("speed set to " + value);
+		}
+	}
+})
 
